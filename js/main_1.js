b80095ec10440989213ee4862c4f16cf1c06e369
tkanfkeo7 <tkanfkeo7@naver.com>
2020-07-13 15:46:56 +0900

    up

--- js/main_1.js
+++ js/main_1.js
@@ -1,46 +1,27 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
 
+    $('#about-section').scroll(function(){
+   
+     var scrollT = $(this).scrollTop(); //스크롤바의 상단위치
+     var scrollH = $(this).height(); //스크롤바를 갖는 div의 높이
+    var contentH = $('#about-section').height(); //문서 전체 내용을 갖는 div의 높이
+    if(scrollT + scrollH +1 >= contentH) { // 스크롤바가 아래 쪽에 위치할 때
+        $(".class_my li .skill_bar li").css{(    'animation-play-state':'running')};
+    });
 
-var isVisible = false;
-
-$(window).on('scroll',function() {
-    if (checkVisible($('#about-section'))&&!isVisible) {
-        /*alert("Visible!!!");*/
-        $(".class_my li .skill_bar li").addClass("bar_ani");
-        isVisible=true;
-    }
+    
 });
 
-function checkVisible( elm, eval ) {
-    eval = eval || "object visible";
-    var viewportHeight = $(window).height(), // Viewport Height
-        scrolltop = $(window).scrollTop(), // Scroll Top
-        y = $(elm).offset().top,
-        elementHeight = $(elm).height();   
+ $(document).ready(function(){
     
-    if (eval == "object visible") return ((y < (viewportHeight + scrolltop)) && (y > (scrolltop - elementHeight)));
-    if (eval == "above") return ((y < (viewportHeight + scrolltop)));
-}
-	});
-/*
-
-
-
-$(document).ready(function(){
+    $(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= Math.ceil($('#about-section').offset().top)) {
+            $(".class_my li .skill_bar li").css{('animation-play-state':'running')};
+            } else {
+            $(".class_my li .skill_bar li").css{('animation-play-state':'paused')};
+            }
+        });
+    });
     
-    var sections = $('#header_main nav li a');
-    var sec001 = sections.attr()
-
-		$('#header_main nav li').each(function(){
-            $(this).click(function(){
-
-			var offset = $('#about-section').offset(); //선택한 태그의 위치를 반환
-
-                //animate()메서드를 이용해서 선택한 태그의 스크롤 위치를 지정해서 0.4초 동안 부드럽게 해당 위치로 이동함 
-
-	        $('html').animate({scrollTop : offset.top}, 400);
-
-		});
-
-	});
-*/
+});*/
